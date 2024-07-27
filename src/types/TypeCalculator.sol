f4bcf542717c12236e1be794f351a5f86a1d6c25
owen <owen@sudorandom.org>
2024-07-27 10:43:37 -0700

    add types stub

--- src/types/TypeCalculator.sol
+++ src/types/TypeCalculator.sol
@@ -8,8 +8,251 @@ import {IEngine} from "../IEngine.sol";
 
 contract TypeCalculator {
 
-    function getTypeEffectiveness(Type attacker, Type defender) public {
-        
+    uint256[16][16] public typeChart;
+
+    constructor() {
+        typeChart[uint256(Type.Yin)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Yin)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Yang)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Yang)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Earth)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Earth)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Water)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Water)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Fire)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Fire)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Metal)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Metal)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Ice)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Ice)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Nature)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Nature)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Lightning)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Lightning)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Mythic)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Mythic)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Air)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Air)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Mind)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Mind)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Cyber)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Cyber)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Wild)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Wild)][uint256(Type.Cosmic)] = 1;
+
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Yin)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Yang)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Earth)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Water)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Fire)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Metal)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Ice)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Nature)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Lightning)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Mythic)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Air)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Mind)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Cyber)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Wild)] = 1;
+        typeChart[uint256(Type.Cosmic)][uint256(Type.Cosmic)] = 1;
     }
 
+    function getTypeEffectiveness(Type attackerType, Type defenderType) public view returns (uint256) {
+        return typeChart[uint256(attackerType)][uint256(defenderType)];
+    }
 }

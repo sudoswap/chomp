ef120d2952f9aadda9acdc46bdee0f274dffee2d
owen <owen@sudorandom.org>
2024-08-25 19:17:49 -0700

    add registry test

--- test/TeamsTest.sol
+++ test/TeamsTest.sol
@@ -29,14 +29,14 @@ contract TeamsTest is Test {
     function setUp() public {
         monRegistry = new DefaultMonRegistry();
         teamRegistry = new DefaultTeamRegistry(
-            DefaultTeamRegistry.Args({REGISTRY: monRegistry, MONS_PER_TEAM: 6, MOVES_PER_MON: 4})
+            DefaultTeamRegistry.Args({REGISTRY: monRegistry, MONS_PER_TEAM: 1, MOVES_PER_MON: 1})
         );  
 
         // Make Alice the mon registry owner
         monRegistry.transferOwnership(ALICE);
     }
 
-    function test_createMon() public {
+    function test_monRegistryFlow() public {
         IAbility ability = new EffectAbility(IEngine(address(0)), IEffect(address(0)));
         IAbility[] memory abilities = new IAbility[](1);
         abilities[0] = ability;
@@ -112,5 +112,70 @@ contract TeamsTest is Test {
         vm.startPrank(BOB);
         vm.expectRevert();
         monRegistry.modifyMon(0, newStats, newMoves, moves, newAbilities, abilities);
+
+        // Assert that only Alice can set additional metadata
+        vm.startPrank(ALICE);
+        bytes32[] memory keys = new bytes32[](1);
+        keys[0] = "test";
+        string[] memory values = new string[](1);
+        values[0] = "test";
+        monRegistry.modifyMonMetadata(0, keys, values);
+
+        // Assert that Bob cannot set additional metadata
+        vm.startPrank(BOB);
+        vm.expectRevert();
+        monRegistry.modifyMonMetadata(0, keys, values);
+    }
+
+    function test_teamRegistryFlow() public {
+        IAbility ability = new EffectAbility(IEngine(address(0)), IEffect(address(0)));
+        IAbility[] memory abilities = new IAbility[](1);
+        abilities[0] = ability;
+
+        IMoveSet move1 = new EffectAttack(IEngine(address(0)), IEffect(address(0)), EffectAttack.Args({
+            TYPE: Type.Fire,
+            STAMINA_COST: 1,
+            PRIORITY: 1
+        }));        
+        
+        IMoveSet move2 = new EffectAttack(IEngine(address(0)), IEffect(address(0)), EffectAttack.Args({
+            TYPE: Type.Fire,
+            STAMINA_COST: 1,
+            PRIORITY: 1
+        }));
+
+        IMoveSet[] memory moves = new IMoveSet[](2);
+        moves[0] = move1;
+        moves[1] = move2;
+
+        MonStats memory stats = MonStats({
+            hp: 1,
+            stamina: 1,
+            speed: 1,
+            attack: 1,
+            defence: 1,
+            specialAttack: 1,
+            specialDefence: 1,
+            type1: Type.Fire,
+            type2: Type.None
+        });
+
+        vm.startPrank(ALICE);
+        monRegistry.createMon(stats, moves, abilities);
+
+        uint256[] memory monIndices = new uint256[](1);
+        monIndices[0] = 0;
+        IMoveSet[][] memory movesToUse = new IMoveSet[][](1);
+        movesToUse[0] = new IMoveSet[](1);
+        movesToUse[0][0] = move1;
+        IAbility[] memory abilitiesToUse = new IAbility[](1);
+        abilitiesToUse[0] = ability;
+        teamRegistry.createTeam(monIndices, movesToUse, abilitiesToUse);
+
+        // Assert the team for Alice exists
+        assertEq(teamRegistry.getTeamCount(ALICE), 1);
+        Mon[] memory aliceTeam0 = teamRegistry.getTeam(ALICE, 0);
+        assertEq(aliceTeam0.length, 1);
+        assertEq(uint(aliceTeam0[0].stats.type1), uint(Type.Fire));
     }
 }

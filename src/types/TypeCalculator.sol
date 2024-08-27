9dc127a00bd8534d7389a5071ec1e57523bbe3a5
owen <22165146+sudo-owen@users.noreply.github.com>
2024-08-27 14:10:05 -0700

    move to python folder

--- src/types/TypeCalculator.sol
+++ src/types/TypeCalculator.sol
@@ -6,8 +6,8 @@ import {ITypeCalculator} from "./ITypeCalculator.sol";
 import "../Enums.sol";
 
 contract TypeCalculator is ITypeCalculator {
-    uint256 private constant MULTIPLIERS_1 = 38597363079105398474523661669562635951089994888546854679819194669304376546645; // First 128 type combinations
-    uint256 private constant MULTIPLIERS_2 = 8369468980515574351781052564276888554803140592618712683861; // Remaining 97 type combinations
+    uint256 private constant MULTIPLIERS_1 = 98387940970013939441334902218489111171706662712193966632938886332804419114328; // First 128 type combinations
+    uint256 private constant MULTIPLIERS_2 = 8859915444081173009646859022170283285020904064171667527525; // Remaining 97 type combinations
 
     function getTypeEffectiveness(Type attackerType, Type defenderType) external pure returns (uint32) {
         uint256 index = uint256(attackerType) * 15 + uint256(defenderType);

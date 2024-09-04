855b1d508520fe63e2abeb05acc3992f2b7e7a77
owen <22165146+sudo-owen@users.noreply.github.com>
2024-09-03 17:32:12 -0700

    wip status effects

---
+++ src/effects/status-effects/IStatusEffect.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import {IEffect} from "../IEffect.sol";
+import {IEngine} from "../../IEngine.sol";
+import {EffectStep} from "../../Enums.sol";
+
+abstract contract IStatusEffect is IEffect {
+    IEngine immutable ENGINE;
+
+    constructor(IEngine _ENGINE) {
+        ENGINE = _ENGINE;
+    }
+
+    bytes32 constant STATUS_EFFECT = "STATUS_EFFECT";
+
+    function _getKeyForMonIndex(uint256 playerIndex, uint256 monIndex) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(STATUS_EFFECT, playerIndex, monIndex));
+    }
+
+    function name() external virtual returns (string memory);
+
+    // Whether to run the effect at a specific step
+    function shouldRunAtStep(EffectStep r) external virtual returns (bool) {}
+
+    // Whether or not to add the effect if the step condition is met
+    function shouldApply(bytes memory, uint256 targetIndex, uint256 monIndex) external virtual returns (bool) {
+        bytes32 battleKey = ENGINE.battleKeyForWrite();
+        bytes32 keyForMon = _getKeyForMonIndex(targetIndex, monIndex);
+
+        // Get value from ENGINE KV
+        bytes32 monStatusFlag = ENGINE.getGlobalKV(battleKey, keyForMon);
+
+        // Check if a status already exists for the mon
+        if (monStatusFlag == bytes32(0)) {
+            ENGINE.setGlobalKV(keyForMon, bytes32("1"));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Lifecycle hooks during normal battle flow
+    function onRoundStart(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData, bool removeAfterRun) {}
+    function onRoundEnd(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData, bool removeAfterRun) {}
+
+    // NOTE: ONLY RUN ON GLOBAL EFFECTS (mons have their Ability as their own hook to apply an effect on switch in)
+    function onMonSwitchIn(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData, bool removeAfterRun) {}
+
+    // NOTE: CURRENTLY ONLY RUN LOCALLY ON MONS (global effects do not have this hook)
+    function onMonSwitchOut(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData, bool removeAfterRun) {}
+
+    // NOTE: CURRENTLY ONLY RUN LOCALLY ON MONS (global effects do not have this hook)
+    function onAfterDamage(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData, bool removeAfterRun) {}
+
+    // Lifecycle hooks when being applied or removed
+    function onApply(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external virtual
+        returns (bytes memory updatedExtraData) {}
+    function onRemove(bytes memory extraData, uint256 targetIndex, uint256 monIndex) external virtual {}
+}
---
+++ src/effects/status-effects/SleepStatus.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import {IEffect} from "../IEffect.sol";
+import {IEngine} from "../../IEngine.sol";
+import {EffectStep, MonStateIndexName} from "../../Enums.sol";
+import {IStatusEffect} from "./IStatusEffect.sol";
+
+contract SleepStatus is IStatusEffect {
+
+    uint256 constant DURATION = 3;
+
+    constructor(IEngine engine) IStatusEffect(engine) {}
+
+    function name() public pure override returns (string memory) {
+        return "Sleep Status Effect";
+    }
+
+    function shouldRunAtStep(EffectStep r) external pure override returns (bool) {
+        return r == EffectStep.RoundStart || r == EffectStep.RoundEnd || r == EffectStep.OnApply;
+    }
+
+    // At the start of the turn, check to see if we should apply sleep or end early
+    function onRoundStart(uint256 rng, bytes memory extraData, uint256 targetIndex, uint256 monIndex)
+        external override
+        returns (bytes memory, bool) {
+        uint256 turnsLeft = abi.decode(extraData, (uint256));
+        bool wakeEarly = rng % 3 == 0;
+        if (turnsLeft == 0 || wakeEarly) {
+            return (extraData, true);
+        }
+        else {
+            _applySleep(rng, targetIndex, monIndex);
+        }
+        return (abi.encode(turnsLeft - 1), false);
+    }
+
+    // On apply, checks to apply the sleep flag, and then sets the extraData to be the duration
+    function onApply(uint256 rng, bytes memory, uint256 targetIndex, uint256 monIndex)
+        external override
+        returns (bytes memory updatedExtraData) {
+        _applySleep(rng, targetIndex, monIndex);
+        return (abi.encode(DURATION));
+    }
+
+    // Sleep just skips the turn
+    function _applySleep(uint256, uint256 targetIndex, uint256 monIndex) internal {
+        ENGINE.updateMonState(targetIndex, monIndex, MonStateIndexName.ShouldSkipTurn, 1);
+    }
+}

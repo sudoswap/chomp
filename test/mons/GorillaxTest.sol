9e4722ed697b22ef80c194d3c1e0da6b375cde9e
owen <22165146+sudo-owen@users.noreply.github.com>
2025-03-08 21:26:47 -0800

    add

--- test/mons/GorillaxTest.sol
+++ test/mons/GorillaxTest.sol
@@ -29,7 +29,11 @@ import {MockRandomnessOracle} from "../mocks/MockRandomnessOracle.sol";
 import {TestTeamRegistry} from "../mocks/TestTeamRegistry.sol";
 import {TestTypeCalculator} from "../mocks/TestTypeCalculator.sol";
 
-contract SofabbiTest is Test, BattleHelper {
+import {CustomEffectAttackFactory} from "../../src/moves/CustomEffectAttackFactory.sol";
+import {CustomEffectAttack} from "../../src/moves/CustomEffectAttack.sol";
+
+contract GorillaxTest is Test, BattleHelper {
+
     Engine engine;
     FastCommitManager commitManager;
     TestTypeCalculator typeCalc;
@@ -37,6 +41,7 @@ contract SofabbiTest is Test, BattleHelper {
     TestTeamRegistry defaultRegistry;
     FastValidator validator;
     Angery angery;
+    CustomEffectAttackFactory attackFactory;
 
     function setUp() public {
         typeCalc = new TestTypeCalculator();
@@ -44,11 +49,14 @@ contract SofabbiTest is Test, BattleHelper {
         defaultRegistry = new TestTeamRegistry();
         engine = new Engine();
         validator = new FastValidator(
-            IEngine(address(engine)), FastValidator.Args({MONS_PER_TEAM: 2, MOVES_PER_MON: 0, TIMEOUT_DURATION: 10})
+            IEngine(address(engine)), FastValidator.Args({MONS_PER_TEAM: 2, MOVES_PER_MON: 2, TIMEOUT_DURATION: 10})
         );
         commitManager = new FastCommitManager(IEngine(address(engine)));
         engine.setCommitManager(address(commitManager));
         angery = new Angery(IEngine(address(engine)));
+        attackFactory = new CustomEffectAttackFactory(
+            new CustomEffectAttack(IEngine(address(engine)), ITypeCalculator(address(typeCalc)))
+        );
     }
 
     /*
@@ -58,4 +66,53 @@ contract SofabbiTest is Test, BattleHelper {
     - Consuming 3 charges correctly heals
     - Consuming only 3 charges (and not more)
     */
+
+    function test_angery() public {
+        // Create a team with a mon that has Angery ability
+        IMoveSet[] memory moves = new IMoveSet[](2);
+        uint256 hpScale = 100;
+
+        // Strong attack is exactly max hp / threshold
+        moves[0] = attackFactory.createAttack(
+            CustomEffectAttackFactory.ATTACK_PARAMS({
+                BASE_POWER: angery.MAX_HP_DENOM * hpScale,
+                STAMINA_COST: 1,
+                ACCURACY: 100,
+                PRIORITY: 1,
+                MOVE_TYPE: Type.Fire,
+                EFFECT: IEffect(address(0)),
+                EFFECT_ACCURACY: 0,
+                MOVE_CLASS: MoveClass.Physical,
+                NAME: "Strong"
+            })
+        );
+        moves[1] = attackFactory.createAttack(
+            CustomEffectAttackFactory.ATTACK_PARAMS({
+                BASE_POWER: angery.MAX_HP_DENOM * hpScale / 2,
+                STAMINA_COST: 1,
+                ACCURACY: 100,
+                PRIORITY: 1,
+                MOVE_TYPE: Type.Fire,
+                EFFECT: IEffect(address(0)),
+                EFFECT_ACCURACY: 0,
+                MOVE_CLASS: MoveClass.Physical,
+                NAME: "Weak"
+            })
+        );
+        Mon memory angeryMon = Mon({
+            stats: MonStats({
+                hp: angery.MAX_HP_DENOM * angery.HP_THRESHOLD_DENOM * hpScale,
+                stamina: 5,
+                speed: 5,
+                attack: 5,
+                defense: 5,
+                specialAttack: 5,
+                specialDefense: 5,
+                type1: Type.Fire,
+                type2: Type.None
+            }),
+            moves: moves,
+            ability: IAbility(address(angery))
+        });
+    }
 }

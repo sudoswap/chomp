d36bc54cdf5322b7169c1b283fcae0cc1054eec5
owen <22165146+sudo-owen@users.noreply.github.com>
2025-05-22 11:47:29 -0700

    wip

--- python/notes.md
+++
@@ -1,5 +0,0 @@
-# Mon Notes
-
-HP rough range: 250-500
-Other stats: 100 (very low) - 400 (very high)
-BST: 1000-1500 (approx range)
--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.0;
 import "forge-std/Script.sol";
 
 // Fundamental entities
+import {IEffect} from "../src/effects/IEffect.sol";
 import {Engine} from "../src/Engine.sol";
 import {FastCommitManager} from "../src/FastCommitManager.sol";
 import {DefaultRuleset} from "../src/DefaultRuleset.sol";
 import {StaminaRegen} from "../src/effects/StaminaRegen.sol";
 import {TypeCalculator} from "../src/types/TypeCalculator.sol";
-import {IEffect} from "../src/effects/IEffect.sol";
+import {DefaultMonRegistry} from "../src/teams/DefaultMonRegistry.sol";
+import {FastValidator} from "../src/FastValidator.sol";
 
 // Important effects
 import {StatBoosts} from "../src/effects/StatBoosts.sol";
@@ -22,40 +24,40 @@ import {ZapStatus} from "../src/effects/status/ZapStatus.sol";
 
 // Mon-specific moves and abilities
 
-// Embursa
-import {SplitThePot} from "../src/mons/embursa/SplitThePot.sol";
-import {Q5} from "../src/mons/embursa/Q5.sol";
-import {HeatBeacon} from "../src/mons/embursa/HeatBeacon.sol";
-import {SetAblaze} from "../src/mons/embursa/SetAblaze.sol";
-import {HoneyBribe} from "../src/mons/embursa/HoneyBribe.sol";
+// // Embursa
+// import {SplitThePot} from "../src/mons/embursa/SplitThePot.sol";
+// import {Q5} from "../src/mons/embursa/Q5.sol";
+// import {HeatBeacon} from "../src/mons/embursa/HeatBeacon.sol";
+// import {SetAblaze} from "../src/mons/embursa/SetAblaze.sol";
+// import {HoneyBribe} from "../src/mons/embursa/HoneyBribe.sol";
 
-// Ghouliath
-import {RiseFromTheGrave} from "../src/mons/ghouliath/RiseFromTheGrave.sol";
-import {Osteoporosis} from "../src/mons/ghouliath/Osteoporosis.sol";
-import {WitherAway} from "../src/mons/ghouliath/WitherAway.sol";
-import {EternalGrudge} from "../src/mons/ghouliath/EternalGrudge.sol";
-import {InfernalFlame} from "../src/mons/ghouliath/InfernalFlame.sol";
+// // Ghouliath
+// import {RiseFromTheGrave} from "../src/mons/ghouliath/RiseFromTheGrave.sol";
+// import {Osteoporosis} from "../src/mons/ghouliath/Osteoporosis.sol";
+// import {WitherAway} from "../src/mons/ghouliath/WitherAway.sol";
+// import {EternalGrudge} from "../src/mons/ghouliath/EternalGrudge.sol";
+// import {InfernalFlame} from "../src/mons/ghouliath/InfernalFlame.sol";
 
-// Gorillax
-import {Angery} from "../src/mons/gorillax/Angery.sol";
-import {Blow} from "../src/mons/gorillax/Blow.sol";
-import {PoundGround} from "../src/mons/gorillax/PoundGround.sol";
-import {RockPull} from "../src/mons/gorillax/RockPull.sol";
-import {ThrowPebble} from "../src/mons/gorillax/ThrowPebble.sol";
+// // Gorillax
+// import {Angery} from "../src/mons/gorillax/Angery.sol";
+// import {Blow} from "../src/mons/gorillax/Blow.sol";
+// import {PoundGround} from "../src/mons/gorillax/PoundGround.sol";
+// import {RockPull} from "../src/mons/gorillax/RockPull.sol";
+// import {ThrowPebble} from "../src/mons/gorillax/ThrowPebble.sol";
 
-// Iblivion
-import {Baselight} from "../src/mons/iblivion/Baselight.sol";
-import {Loop} from "../src/mons/iblivion/Loop.sol";
-import {FirstResort} from "../src/mons/iblivion/FirstResort.sol";
-import {Brightback} from "../src/mons/iblivion/Brightback.sol";
-import {IntrinsicValue} from "../src/mons/iblivion/IntrinsicValue.sol";
+// // Iblivion
+// import {Baselight} from "../src/mons/iblivion/Baselight.sol";
+// import {Loop} from "../src/mons/iblivion/Loop.sol";
+// import {FirstResort} from "../src/mons/iblivion/FirstResort.sol";
+// import {Brightback} from "../src/mons/iblivion/Brightback.sol";
+// import {IntrinsicValue} from "../src/mons/iblivion/IntrinsicValue.sol";
 
-// Inutia
-import {Interweaving} from "../src/mons/inutia/Interweaving.sol";
-import {ShrineStrike} from "../src/mons/inutia/ShrineStrike.sol";
-import {Initialize} from "../src/mons/inutia/Initialize.sol";
-import {ChainExpansion} from "../src/mons/inutia/ChainExpansion.sol";
-import {BigBite} from "../src/mons/inutia/BigBite.sol";
+// // Inutia
+// import {Interweaving} from "../src/mons/inutia/Interweaving.sol";
+// import {ShrineStrike} from "../src/mons/inutia/ShrineStrike.sol";
+// import {Initialize} from "../src/mons/inutia/Initialize.sol";
+// import {ChainExpansion} from "../src/mons/inutia/ChainExpansion.sol";
+// import {BigBite} from "../src/mons/inutia/BigBite.sol";
 
 contract Deploy is Script {
     function run()
@@ -64,23 +66,35 @@ contract Deploy is Script {
             Engine engine,
             FastCommitManager commitManager,
             TypeCalculator typeCalc,
+            DefaultMonRegistry monRegistry,
+            FastValidator validator,
+            StaminaRegen staminaRegen,
+            DefaultRuleset ruleset,
+
             StatBoosts statBoosts,
             Storm storm,
             SleepStatus sleepStatus,
             PanicStatus panicStatus,
             FrostbiteStatus frostbiteStatus,
             BurnStatus burnStatus,
-            ZapStatus zapStatus,
-            StaminaRegen staminaRegen,
-            DefaultRuleset ruleset
+            ZapStatus zapStatus
         )
     {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         vm.startBroadcast(deployerPrivateKey);
         engine = new Engine();
-        // commitManager = new FastCommitManager(engine);
-        // engine.setCommitManager(address(commitManager));
-        // typeCalc = new TypeCalculator();
+        commitManager = new FastCommitManager(engine);
+        engine.setCommitManager(address(commitManager));
+        typeCalc = new TypeCalculator();
+        monRegistry = new DefaultMonRegistry();
+        staminaRegen = new StaminaRegen(engine);
+        IEffect[] memory effects = new IEffect[](1);
+        effects[0] = staminaRegen;
+        ruleset = new DefaultRuleset(engine, effects);
+        validator = new FastValidator(
+            engine, FastValidator.Args({MONS_PER_TEAM: 4, MOVES_PER_MON: 4, TIMEOUT_DURATION: 30})
+        );
+
         // statBoosts = new StatBoosts(engine);
         // storm = new Storm(engine, statBoosts);
         // sleepStatus = new SleepStatus(engine);
@@ -88,10 +102,6 @@ contract Deploy is Script {
         // frostbiteStatus = new FrostbiteStatus(engine, statBoosts);
         // burnStatus = new BurnStatus(engine, statBoosts);
         // zapStatus = new ZapStatus(engine);
-        // staminaRegen = new StaminaRegen(engine);
-        // IEffect[] memory effects = new IEffect[](1);
-        // effects[0] = staminaRegen;
-        // ruleset = new DefaultRuleset(engine, effects);
         vm.stopBroadcast();
     }
 }
